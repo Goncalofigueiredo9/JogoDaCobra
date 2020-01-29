86c4eb6eff83d2c202b93e2f5e8b227f122cf2ab
Gonçalo Figueiredo <goncalofigueira53@gmail.com>
2020-01-29 12:04:35 +0000

    icon

--- JogoDaCobra/Form1.Designer.cs
+++ JogoDaCobra/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -132,6 +133,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pbCanvas);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Jogo da Cobra";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
